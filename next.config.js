cfee7921958ad89f362b733219f81148bdcca040
Sean Sáez Fuller <sean.saez.f@gmail.com>
2022-10-10 11:51:11 +0200

    Poke app finalizada

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: {
+    domains: ['raw.githubusercontent.com']
+  }
 }
-
-module.exports = nextConfig
